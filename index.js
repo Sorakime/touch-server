32228dd398dc65a63b19df49c9368f00bc8c0e43
Sorakime <69241694+Sorakime@users.noreply.github.com>
2021-08-30 07:01:48 +0900

    v.1.0.0 beta 3

--- index.js
+++ index.js
@@ -1,48 +1,49 @@
 var http=require('http');
 var mime=require('mime-types');
 var fs=require('fs');
-var port=null;
-if(process.argv[2]>1024){
-	port=process.argv[2];
-}else if(process.argv[2]<1024){
+var port;
+if(Number(process.argv[2])>=1024){
+	port=Number(process.argv[2]);
+}else if(Number(process.argv[2])<1024){
+	console.log('Changed: The port number was changed to 1024 because your inputted number will not do work.');
 	port=1024;
+}else{
+	port=3000;
 }
+
 var server=http.createServer();
+
+function exists(path){
+	try{
+		fs.readFileSync(path, 'utf-8');
+		return true;
+	}catch(e){
+		return false;
+	}
+}
 
 //process
 server.on('request',(req, res)=>{
 	try{
-		var status, path;
 
-		if(fs.existsSync(process.cwd()+req.url)){
-			status=200;
- 			path=process.cwd()+req.url;
-		}else if(fs.existsSync(process.cwd()+req.url+'/index.html')){
-			status=200;
-			path=process.cwd()+req.url+'/index.html';
-		}else{
-			status=404;
-			path=`${__dirname}/404.html`;
-		}
-
-		res.writeHead(status, {'Content-Type': mime.lookup(path)});
-		res.write(fs.readFileSync(path, 'utf-8'));
-		res.end();
-	}catch(e){
-		if(fs.existsSync(process.cwd()+req.url+'/index.html')){
+		if(exists(process.cwd()+req.url)){
+			res.writeHead(200, {'Content-Type': mime.lookup(process.cwd()+req.url)});
+			res.write(fs.readFileSync(process.cwd()+req.url, 'utf-8'));
+		}else if(exists(process.cwd()+req.url+'/index.html')){
 			res.writeHead(200, {'Content-Type': mime.lookup(process.cwd()+req.url+'/index.html')});
-			res.write(fs.readFileSync(process.cwd()+req.url+'/index.html'));
-			res.end();
+			res.write(fs.readFileSync(process.cwd()+req.url+'/index.html','utf-8'));
 		}else{
-			res.writeHead(500, {'Content-Type': 'text/html'});
-			res.write(fs.readFileSync('./500.html','utf-8'));
-			res.end();
+			res.writeHead(404, {'Content-Type': 'text/html'});
+			res.write(fs.readFileSync(`${__dirname}/404.html`,'utf-8'));
 		}
+
+	}catch(e){
+		res.writeHead(500, {'Content-Type': 'text/html'});
+		res.write(fs.readFileSync(`${__dirname}/500.html`,'utf-8'));
+		console.log(e);
+	}finally{
+		res.end();
 	}
 })
 
-if(port!=null){
-	server.listen(port);
-}else{
-	server.listen(3000);
-}
+server.listen(port);
